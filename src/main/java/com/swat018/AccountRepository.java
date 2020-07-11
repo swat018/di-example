923099ee62a77239051396aa552d09a187fab524
jinwoo park <swat018@gmail.com>
2020-07-11 09:01:36 +0900

    나만의 DI 프레임워크 만들기

---
+++ src/main/java/com/swat018/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.swat018;
+
+public class AccountRepository {
+
+    public void save() {
+        System.out.println("Repo.save");
+    }
+}
--- src/main/java/com/swat018/App.java
+++ src/main/java/com/swat018/App.java
@@ -1,5 +1,7 @@
 package com.swat018;
 
+import com.swat018.di.ContainerService;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AccountService accountService = ContainerService.getObject(AccountService.class);
+        accountService.join();
     }
 }
